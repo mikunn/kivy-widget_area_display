bcffe97148484bda6112d9c957fdc6e59318413c
mikunn <mikaj@rocketmail.com>
2014-04-20 13:29:37 +0300

    Initial version

---
+++ wadisp.py
@@ -0,0 +1,67 @@
+from kivy.uix.widget import Widget
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.graphics.context_instructions import Color
+
+class InvalidColor(Exception):
+    pass
+
+def show_widget_area(self, color='green', alpha=0.5, group=None):
+    '''
+    Draws a rectangle on the widget's canvas 
+    to display its position and proportions.
+    '''
+    
+    max_ = 255.0
+    
+    color_names = {'blue': [0, 0, 1],
+                   'cyan': [0, 1, 1],
+                   'green': [0, 1, 0],
+                   'fuchsia': [1, 0, 1],
+                   'orange': [1, 165/max_, 0],
+                   'pink': [1, 192/max_, 203/max_],
+                   'red': [1, 0, 0],
+                   'yellow': [1, 1, 0],
+                   }
+    rgba = None
+    
+    if type(color) == str:
+        
+        for name in color_names:
+            if name.startswith(color):
+                rgba = color_names[name]
+        
+        try:
+            rgba += [alpha]
+        
+        except TypeError:
+            raise InvalidColor('No color match for color string "' + color + '"')
+    
+    elif isinstance(color, (list, tuple)):
+        if len(color) == 4:
+            rgba = color
+        
+        else:
+            rgba = list(color) + [alpha]
+    
+    attr = self.canvas
+    
+    if group == 'after':
+        attr = self.canvas.after
+    
+    elif group == 'before':
+        attr = self.canvas.before
+        
+    with attr:
+        Color(*rgba)
+        self.area = Rectangle(pos=self.pos, size=self.size)
+    
+    self.bind(pos=self.update_rect)
+    self.bind(size=self.update_rect)
+
+def update_area(self, *args):
+    self.area.pos = self.pos
+    self.area.size = self.size
+
+def attach():
+    Widget.update_rect = update_area
+    Widget.show_widget_area = show_widget_area
---
+++ widgetareaexample.py
@@ -0,0 +1,67 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+import wadisp
+
+Builder.load_string('''
+
+<MyBoxLayout>:
+    orientation: 'vertical'
+    
+    GreenLabel:
+        text: 'GreenLabel'
+        canvas: self.show_widget_area()
+        
+    BlueLabel:
+        text: 'BlueLabel'
+        canvas: self.show_widget_area(color='b')
+    
+    GridLayout:
+        rows: 1
+        canvas: self.show_widget_area(color='orange', alpha=1, group='before')
+        
+        CustomColorLabel:
+            size_hint: None, None
+            size: 200, 40
+            text: 'CustomColorLabel'
+            canvas: self.show_widget_area(color=(.2, .7, .8, 1))
+        
+        CustomColorButton:
+            size_hint: None, None
+            size: 200, 40
+            text: 'CustomColorButton'
+        
+        AnotherCustomColorButton:
+            size_hint: None, None
+            size: 200, 40
+            text: 'AnotherCustomColorButton'
+            
+<GreenLabel@Label>:
+<BlueLabel@Label>:
+<CustomColorLabel@Label>:
+
+''')
+
+class WidgetAreaTestApp(App):
+    def build(self):
+        return MyBoxLayout()
+
+class MyBoxLayout(BoxLayout):
+    pass
+
+class CustomColorButton(Button):
+    def __init__(self, **kwargs):
+        super(CustomColorButton, self).__init__()
+        self.show_widget_area(color=(0,1,1), alpha=0.8)
+        
+class AnotherCustomColorButton(Button):
+    def __init__(self, **kwargs):
+        super(AnotherCustomColorButton, self).__init__()
+        self.show_widget_area(color=(0.7, 0.7, 0.3), alpha=0.8, group='after')
+    
+if __name__ == '__main__':
+    wadisp.attach()
+    WidgetAreaTestApp().run()
+    
